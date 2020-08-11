760655ca2d115fa7e371edb970264f75333bf398
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-11 02:54:52 +0000

    Automatically backed up by Learn

--- src/ColorBox.js
+++ src/ColorBox.js
@@ -4,17 +4,16 @@ export default class ColorBox extends Component {
 
   state = {
     todos: [
-      
+
     ]
   }
 
   render() {
     return (
       <div className="color-box" style={{opacity: null /*replace null with the value*/}}>
-        {/* your conditional code here! */}
+        {this.props.opacity}
       </div>
     )
   }
 
 }
-
