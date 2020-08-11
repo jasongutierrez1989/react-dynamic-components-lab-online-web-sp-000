86af8e7e72d57f9ba0730b5975e1f0e69da9908e
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2020-08-11 02:56:03 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -2,12 +2,6 @@ import React, { Component } from 'react';
 
 export default class Comment extends Component {
 
-  state = {
-    todos: [
-
-    ]
-  }
-
   render() {
     return (
       <div className="comment">
